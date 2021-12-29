2ff7b4a49ed32695b164579e2eaef2960b23f160
zorluozan <61868614+zorluozan@users.noreply.github.com>
2021-12-29 17:31:14 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,request
+import requests
+
+api_key = "<your_api_key>"
+
+url = "http://data.fixer.io/api/latest?access_key=" + api_key
+
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        firstCurrency = request.form.get("firstCurrency")
+        secondCurrency = request.form.get("secondCurrency")
+
+        amount = request.form.get("amount")
+        response = requests.get(url)
+
+        infos = response.json()
+
+        firstValue = infos["rates"][firstCurrency]
+        secondValue = infos["rates"][secondCurrency]
+
+        result = (secondValue / firstValue) * float(amount)
+
+        currencyInfo = dict()
+
+        currencyInfo["firstCurrency"] = firstCurrency
+        currencyInfo["secondCurrency"] = secondCurrency
+        currencyInfo["amount"] = amount
+        currencyInfo["result"] = result
+
+        return render_template("index.html",info = currencyInfo)
+
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
